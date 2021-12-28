cf23f631944c44c85385566a91660f128ae7fc3d
Uttam Marandi <uttammarandi007@gmail.com>
2021-12-28 20:21:14 +0530

    Header component

--- screens/Home/Home.js
+++ screens/Home/Home.js
@@ -1,10 +1,64 @@
 import React from "react";
-import { View, Text } from "react-native";
+import {
+  View,
+  Text,
+  TouchableOpacity,
+  Image,
+  TextComponent,
+  FlatList,
+  TextInput,
+} from "react-native";
+import { FONTS, SIZES, COLORS, icons, dummyData } from "../../constants";
 
 const Home = () => {
+  function renderSearch() {
+    return (
+      <View
+        style={{
+          flexDirection: "row",
+          height: 40,
+          alignItems: "center",
+          marginHorizontal: SIZES.padding,
+          marginVertical: SIZES.base,
+          paddingHorizontal: SIZES.radius,
+          borderRadius: SIZES.radius,
+          backgroundColor: COLORS.lightGray2,
+        }}
+      >
+        {/* Icon */}
+        <Image
+          source={icons.search}
+          style={{ height: 20, width: 20, tintColor: COLORS.black }}
+        />
+        {/* Text Input */}
+        <TextInput
+          style={{
+            flex: 1,
+            paddingTop: 0,
+            paddingBottom: 0,
+            marginLeft: SIZES.radius,
+            ...FONTS.body3,
+          }}
+          placeholder="search food ..."
+        />
+
+        {/* Filter button */}
+
+        <TouchableOpacity>
+          <Image
+            source={icons.filter}
+            style={{ height: 20, width: 20, tintColor: COLORS.black }}
+          />
+        </TouchableOpacity>
+      </View>
+    );
+  }
+
   return (
-    <View>
-      <Text>Home</Text>
+    <View style={{ flex: 1 }}>
+      {/* Search */}
+      {renderSearch()}
+      {/* List */}
     </View>
   );
 };
