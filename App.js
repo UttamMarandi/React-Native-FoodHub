0ba1f4d4457769a0b78f1175574db72376743910
Uttam Marandi <uttammarandi007@gmail.com>
2021-12-28 17:58:26 +0530

    shared values and animated style

--- App.js
+++ App.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { createStackNavigator } from "@react-navigation/stack";
 import { NavigationContainer } from "@react-navigation/native";
+import "react-native-gesture-handler";
 
 import { createStore, applyMiddleware } from "redux";
 import { Provider } from "react-redux";
--- screens/MainLayout.js
+++ screens/MainLayout.js
@@ -27,10 +27,120 @@ import {
   dummyData,
 } from "../constants";
 
+const TabButton = ({ label, icon, isFocused, onPress }) => {
+  return (
+    <TouchableWithoutFeedback onPress={onPress}>
+      <Animated.View
+        style={{ flex: 1, alignItems: "center", justifyContent: "center" }}
+      >
+        <Animated.View
+          style={{
+            flexDirection: "row",
+            width: "80%",
+            height: 50,
+            alignItems: "center",
+            justifyContent: "center",
+            borderRadius: 25,
+          }}
+        >
+          <Image
+            source={icon}
+            style={{ width: 20, height: 20, tintColor: COLORS.gray }}
+          />
+          {isFocused && (
+            <Text
+              numberOfLines={1}
+              style={{
+                marginLeft: SIZES.base,
+                color: COLORS.gray,
+                ...FONTS.h3,
+              }}
+            >
+              {label}
+            </Text>
+          )}
+        </Animated.View>
+      </Animated.View>
+    </TouchableWithoutFeedback>
+  );
+};
+
 const MainLayout = ({ navigation, selectedTab, setSelectedTab }) => {
   useEffect(() => {
     setSelectedTab(constants.screens.home);
   }, []);
+  //Tab Animation
+
+  //====>Reanimated shared value
+  const homeTabFlex = useSharedValue(1);
+  const homeTabColor = useSharedValue(COLORS.white);
+  const searchTabFlex = useSharedValue(1);
+  const searchTabColor = useSharedValue(COLORS.white);
+  const cartTabFlex = useSharedValue(1);
+  const cartTabColor = useSharedValue(COLORS.white);
+  const favouriteTabFlex = useSharedValue(1);
+  const favouriteTabColor = useSharedValue(COLORS.white);
+  const notificationTabFlex = useSharedValue(1);
+  const notificationTabColor = useSharedValue(COLORS.white);
+
+  //===> Reanimated animated style
+  const homeFlexStyle = useAnimatedStyle(() => {
+    return {
+      flex: homeTabFlex.value,
+    };
+  });
+
+  const homeColorStyle = useAnimatedStyle(() => {
+    return {
+      backgroundColor: homeTabColor.value,
+    };
+  });
+
+  const searchFlexStyle = useAnimatedStyle(() => {
+    return {
+      flex: searchTabFlex.value,
+    };
+  });
+
+  const searchColorStyle = useAnimatedStyle(() => {
+    return {
+      backgroundColor: searchTabColor.value,
+    };
+  });
+
+  const cartFlexStyle = useAnimatedStyle(() => {
+    return {
+      flex: cartTabFlex.value,
+    };
+  });
+
+  const cartColorStyle = useAnimatedStyle(() => {
+    return {
+      backgroundColor: cartTabColor.value,
+    };
+  });
+
+  const favouriteFlexStyle = useAnimatedStyle(() => {
+    return {
+      flex: favouriteTabFlex.value,
+    };
+  });
+  const favouriteColorStyle = useAnimatedStyle(() => {
+    return {
+      backgroundColor: favouriteTabColor.value,
+    };
+  });
+
+  const notificationFlexStyle = useAnimatedStyle(() => {
+    return {
+      flex: notificationTabFlex.value,
+    };
+  });
+  const notificationColorStyle = useAnimatedStyle(() => {
+    return {
+      backgroundColor: notificationTabColor.value,
+    };
+  });
 
   // Drawer Animation
   const progress = useDrawerProgress();
@@ -105,9 +215,10 @@ const MainLayout = ({ navigation, selectedTab, setSelectedTab }) => {
 
       {/* Footer */}
       <View style={{ height: 100, justifytontent: "flex-end" }}>
+        {/* Shadow */}
         <LinearGradient
           start={{ x: 0, y: 0 }}
-          end={{ x: 0, y: 4 }}
+          end={{ x: 0, y: 1 }}
           colors={[COLORS.transparent, COLORS.lightGray1]}
           style={{
             position: "absolute",
@@ -119,6 +230,63 @@ const MainLayout = ({ navigation, selectedTab, setSelectedTab }) => {
             borderTopRightRadius: 15,
           }}
         />
+        {/* Tabs */}
+        <View
+          style={{
+            flex: 1,
+            flexDirection: "row",
+            paddingHorizontal: SIZES.radius,
+            paddingBottom: 10,
+            borderTopLeftRadius: 20,
+            borderTopRightRadius: 20,
+            backgroundColor: COLORS.white,
+          }}
+        >
+          <TabButton
+            label={constants.screens.home}
+            icon={icons.home}
+            isFocused={selectedTab == constants.screens.home}
+            onPress={() => setSelectedTab(constants.screens.home)}
+            outerContainerStyle={homeFlexStyle}
+            innerContainerStyle={homeColorStyle}
+          />
+
+          <TabButton
+            label={constants.screens.search}
+            icon={icons.search}
+            isFocused={selectedTab == constants.screens.search}
+            onPress={() => setSelectedTab(constants.screens.search)}
+            outerContainerStyle={searchFlexStyle}
+            innerContainerStyle={searchColorStyle}
+          />
+
+          <TabButton
+            label={constants.screens.cart}
+            icon={icons.cart}
+            isFocused={selectedTab == constants.screens.cart}
+            onPress={() => setSelectedTab(constants.screens.cart)}
+            outerContainerStyle={cartFlexStyle}
+            innerContainerStyle={cartColorStyle}
+          />
+
+          <TabButton
+            label={constants.screens.favourite}
+            icon={icons.favourite}
+            isFocused={selectedTab == constants.screens.favourite}
+            onPress={() => setSelectedTab(constants.screens.favourite)}
+            outerContainerStyle={favouriteFlexStyle}
+            innerContainerStyle={favouriteColorStyle}
+          />
+
+          <TabButton
+            label={constants.screens.notification}
+            icon={icons.notification}
+            isFocused={selectedTab == constants.screens.notification}
+            onPress={() => setSelectedTab(constants.screens.notification)}
+            outerContainerStyle={notificationFlexStyle}
+            innerContainerStyle={notificationColorStyle}
+          />
+        </View>
       </View>
     </Animated.View>
   );
@@ -140,3 +308,30 @@ export default connect(mapStateToProps, mapDispatchToProps)(MainLayout);
 // Bug fix
 
 //In order to render the shadow effect using <LinearGradient/> we have to remove alignItems and justifyContent from parent container
+
+//Bug Fix
+
+//Problem
+//Reanimated 2 failed to create a worklet, maybe you forgot to add Reanimated's babel plugin?
+
+//Solution
+//Installation and configuaration failure of Reanimated 2
+//Install latest / any version of reanimated 2 above 2.0
+//babel.config.js
+//add ==> "react-native-reanimated/plugin" // this should be the last plugin registerd
+
+//Turn on Hermes engine by editing android/app/build.gradle
+//project.ext.react = [
+//   enableHermes: true  // <- here | clean and rebuild if changing
+// ]
+
+//Plug Reanimated in MainApplication.java
+//add==>
+//  import com.facebook.react.bridge.JSIModulePackage; // <- add
+// import com.swmansion.reanimated.ReanimatedJSIModulePackage; // <- add
+
+//Inside ReactNativeHost(this)  method
+//@Override
+// protected JSIModulePackage getJSIModulePackage() {
+//   return new ReanimatedJSIModulePackage(); // <- add
+// }
