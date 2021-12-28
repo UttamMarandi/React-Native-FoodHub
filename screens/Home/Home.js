f826d50c13f0973427cc32cc2e2e3994bd164b9d
Uttam Marandi <uttammarandi007@gmail.com>
2021-12-28 23:08:02 +0530

    Horizental food card

--- screens/Home/Home.js
+++ screens/Home/Home.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import {
   View,
   Text,
@@ -10,7 +10,65 @@ import {
 } from "react-native";
 import { FONTS, SIZES, COLORS, icons, dummyData } from "../../constants";
 
+import { HorizontalFoodCard } from "../../components";
+
+const Section = ({ title, onPress, children }) => {
+  return (
+    <View>
+      {/* Header */}
+      <View
+        style={{
+          flexDirection: "row",
+          marginHorizontal: SIZES.padding,
+          marginTop: 30,
+          marginBottom: 20,
+        }}
+      >
+        <Text style={{ flex: 1, ...FONTS.h3 }}>{title}</Text>
+
+        <TouchableOpacity onPress={onPress}>
+          <Text style={{ color: COLORS.primary, ...FONTS.body3 }}>
+            Show all
+          </Text>
+        </TouchableOpacity>
+      </View>
+
+      {/* COntent */}
+      {children}
+    </View>
+  );
+};
+
 const Home = () => {
+  const [selectedCategoryId, setSelectedCategoryId] = useState(1);
+  const [selectedMenuType, setSelectedMenuType] = useState(1);
+  const [recommends, setRecommends] = useState([]);
+  const [menuList, setMenuList] = useState([]);
+
+  useEffect(() => {
+    handleChangeCategory(selectedCategoryId, selectedMenuType);
+  }, []);
+
+  // Handler
+
+  function handleChangeCategory(categoryId, menuTypeId) {
+    //Retrieve the recommended menu
+    let selectedRecommend = dummyData.menu.find((a) => a.name == "Recommended");
+
+    //Find the menu based on menu type id
+    let selectedMenu = dummyData.menu.find((a) => a.id == menuTypeId);
+
+    //Set the recommended menu based on the categoryid
+    setRecommends(
+      selectedRecommend?.list.filter((a) => a.categories.includes(categoryId))
+    );
+
+    //Set the menu based on category id
+    setMenuList(
+      selectedMenu?.list.filter((a) => a.categories.includes(categoryId))
+    );
+  }
+
   function renderSearch() {
     return (
       <View
@@ -54,13 +112,126 @@ const Home = () => {
     );
   }
 
+  function renderMenuTypes() {
+    return (
+      <FlatList
+        horizontal
+        data={dummyData.menu}
+        keyExtractor={(item) => `${item.id}`}
+        showsHorizontalScrollIndicator={false}
+        contentContainerStyle={{
+          marginTop: 30,
+          marginBottom: 20,
+        }}
+        renderItem={({ item, index }) => (
+          <TouchableOpacity
+            style={{
+              marginLeft: SIZES.padding,
+              marginRight:
+                index == dummyData.menu.length - 1 ? SIZES.padding : 0,
+            }}
+            onPress={() => {
+              setSelectedMenuType(item.id);
+              handleChangeCategory(selectedCategoryId, item.id);
+            }}
+          >
+            <Text
+              style={{
+                color:
+                  selectedMenuType == item.id ? COLORS.primary : COLORS.black,
+                ...FONTS.h3,
+              }}
+            >
+              {item.name}
+            </Text>
+          </TouchableOpacity>
+        )}
+      />
+    );
+  }
+
+  function renderRecommendedSection() {
+    return (
+      <Section
+        title="Recommended"
+        onPress={() => console.log("Show all recommended")}
+      >
+        <FlatList
+          data={recommends}
+          keyExtractor={(item) => `${item.id}`}
+          horizontal
+          showsHorizontalScrollIndicator={false}
+          renderItem={({ item, index }) => (
+            <HorizontalFoodCard
+              containerStyle={{
+                height: 180,
+                width: SIZES.width * 0.85,
+                marginLeft: index == 0 ? SIZES.padding : 18, //index ==0 means it is the first element of flallist
+                marginRight: index == recommends.length - 1 ? SIZES.padding : 0,
+                // index == recommends.length - 1 means it is the last element of flatlist
+                paddingRight: SIZES.radius,
+                alignItems: "center",
+              }}
+              imageStyle={{
+                marginTop: 35,
+                height: 150,
+                width: 150,
+              }}
+              item={item}
+              onPress={() => console.log("Horizental food card")}
+            />
+          )}
+        />
+      </Section>
+    );
+  }
   return (
     <View style={{ flex: 1 }}>
       {/* Search */}
       {renderSearch()}
       {/* List */}
+
+      <FlatList
+        data={menuList}
+        keyExtractor={(item) => `${item.id}`}
+        showsHorizontalScrollIndicator={false}
+        ListHeaderComponent={
+          <View>
+            {/* Recommended */}
+
+            {renderRecommendedSection()}
+
+            {/* Menu Type */}
+            {renderMenuTypes()}
+          </View>
+        }
+        renderItem={({ item, index }) => {
+          return (
+            <HorizontalFoodCard
+              containerStyle={{
+                height: 130,
+                alignItems: "center",
+                marginHorizontal: SIZES.padding,
+                marginBottom: SIZES.radius,
+              }}
+              imageStyle={{
+                marginTop: 20,
+                height: 110,
+                width: 110,
+              }}
+              item={item}
+              onPress={() => console.log("Horizental Food Card")}
+            />
+          );
+        }}
+      />
     </View>
   );
 };
 
 export default Home;
+
+// bug Fix : Inputtext alignment, Input text was top aligned
+
+// Solution :
+// set paddingTop and paddingBottom to 0
