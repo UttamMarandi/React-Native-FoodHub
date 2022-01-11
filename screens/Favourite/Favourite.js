297919946e8d73db091bd3292506d2730d44664b
Uttam Marandi <uttammarandi007@gmail.com>
2022-01-11 11:41:25 +0530

    notification page done

--- screens/Favourite/Favourite.js
+++ screens/Favourite/Favourite.js
@@ -1,6 +1,33 @@
 import React from "react";
 import { View, Text } from "react-native";
 
+const Section = ({ title, onPress, children }) => {
+  return (
+    <View>
+      {/* Header */}
+      <View
+        style={{
+          flexDirection: "row",
+          marginHorizontal: SIZES.padding,
+          marginTop: 30,
+          marginBottom: 20,
+        }}
+      >
+        <Text style={{ flex: 1, ...FONTS.h3 }}>{title}</Text>
+
+        <TouchableOpacity onPress={onPress}>
+          <Text style={{ color: COLORS.primary, ...FONTS.body3 }}>
+            Show all
+          </Text>
+        </TouchableOpacity>
+      </View>
+
+      {/* COntent */}
+      {children}
+    </View>
+  );
+};
+
 const Favourite = () => {
   return (
     <View>
--- screens/Notification/Notification.js
+++ screens/Notification/Notification.js
@@ -1,10 +1,47 @@
 import React from "react";
-import { View, Text } from "react-native";
+import { View, Text, Image } from "react-native";
+import { COLORS, FONTS, images, SIZES } from "../../constants";
 
 const Notification = () => {
   return (
-    <View>
-      <Text>Notification</Text>
+    <View
+      style={{
+        flex: 1,
+        paddingHorizontal: SIZES.padding,
+        backgroundColor: COLORS.white,
+        marginBottom: 250,
+      }}
+    >
+      <View
+        style={{
+          flex: 1,
+          alignItems: "center",
+          justifyContent: "center",
+        }}
+      >
+        <Image
+          source={images.notification_image}
+          resizeMode="contain"
+          style={{
+            width: 450,
+            height: 500,
+            position: "relative",
+            top: 0,
+            right: 15,
+          }}
+        />
+
+        <Text
+          style={{
+            textAlign: "center",
+            marginTop: SIZES.base,
+            color: COLORS.darkGray,
+            ...FONTS.body3,
+          }}
+        >
+          You have not received any notification yet.
+        </Text>
+      </View>
     </View>
   );
 };
